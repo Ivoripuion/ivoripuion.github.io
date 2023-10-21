519beb3546cd2052a18031f6d1a307807f9a9303
ivoripuion <1049917327@qq.com>
2023-10-22 01:09:10 +0800

    Site updated: 2023-10-22 01:09:07

--- js/utils.js
+++ js/utils.js
@@ -605,15 +605,15 @@ const anzhiyu = {
       let message = "";
 
       if (hour >= 0 && hour <= 5) {
-        message = "睡个好觉，保证精力充沛";
+        message = "是时候休息了。";
       } else if (hour > 5 && hour <= 10) {
-        message = "一日之计在于晨";
+        message = "早安！";
       } else if (hour > 10 && hour <= 14) {
-        message = "吃饱了才有力气干活";
+        message = "慵懒的上午随便打发打发时间吧。";
       } else if (hour > 14 && hour <= 18) {
-        message = "集中精力，攻克难关";
+        message = "希望下午的时间会议会少一些。";
       } else if (hour > 18 && hour <= 24) {
-        message = "不要太劳累了，早睡更健康";
+        message = "现在是娱乐时间。";
       }
 
       return message;
